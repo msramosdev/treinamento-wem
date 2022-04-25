fa66745ff07609b1063565fec235e906862d4a70
Miguel Ramos <miguel.ramos@vilt.local>
2022-04-25 11:37:15 -0300

    Includes Spring configurations

---
+++ src/main/java/com/vilt/trainningwem/TrainningWemApplication.java
@@ -0,0 +1,13 @@
+package com.vilt.trainningwem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainningWemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainningWemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilt/trainningwem/TrainningWemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilt.trainningwem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainningWemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
